d621df21fa11ee1c432b0647fcb69fb926bf6081
Cheska Huang <julyannahuang@gmail.com>
2026-07-12 00:33:52 +0800

    initial screen layout

--- app/layouts/ScreenLayout.tsx
+++ app/layouts/ScreenLayout.tsx
@@ -1,1 +1,19 @@
 import { Outlet } from "react-router";
+
+export function ScreenLayout() {
+	const fullScreenLayer =
+		"pointer-events-none bg-center bg-cover w-full h-full fixed inset-0";
+	return (
+		<main className="w-screen min-h-screen">
+			<div className="radial-bg w-full min-h-screen fixed inset-0">
+				<Outlet />
+			</div>
+			<div
+				className={`${fullScreenLayer} bg-[url('/screen-layout/Screen Border.svg')]`}
+			/>
+			<div
+				className={`${fullScreenLayer} bg-[url('/screen-layout/texture.svg')]`}
+			/>
+		</main>
+	);
+}
