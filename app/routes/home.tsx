278e6bc08c9894e5030a3f8013eb77994569e38c
Cheska Huang <julyannahuang@gmail.com>
2026-07-12 10:25:44 +0800

    initial intro

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
-import type { Route } from "./+types/home";
+import Intro from "~/intro/Intro";
 import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export function meta() {
+	return [
+		{ title: "If these walls could talk" },
+		{ name: "description", content: "Welcome to React Router!" },
+	];
 }
 
 export default function Home() {
-  return <Welcome />;
+	return <Intro />;
 }
