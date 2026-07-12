a3c9dd1d1015d47e261536bbc7a90e88a25cef53
Cheska Huang <julyannahuang@gmail.com>
2026-07-12 10:39:52 +0800

    initial intro

--- app/intro/Intro.tsx
+++ app/intro/Intro.tsx
@@ -1,4 +1,19 @@
-export function LoadingBar({ timer }: { timer: number }) {}
+export function LoadingBar({ timer }: { timer: number }) {
+	return <></>;
+}
+
+export function LoadingScreen() {
+	return (
+		<div className="flex h-screen w-screen items-center justify-center">
+			<p className="font-mono text-white text-xl text-shadow text-center w-1/2">
+				Trigger warning: This interactive article contains
+				<br />
+				mentions of sexual harassment
+			</p>
+			<LoadingBar timer={0} />
+		</div>
+	);
+}
 
 export default function Intro() {
 	return (
@@ -6,7 +21,9 @@ export default function Intro() {
 			{/* scroll container */}
 			<section className="w-screen min-h-screen overflow-x-clip overflow-y-auto">
 				{/* container for animations */}
-				<section className="radial-bg w-screen h-screen grid grid-cols-1 grid-rows-1 place-items-center"></section>
+				<section className="radial-bg w-screen h-screen grid grid-cols-1 grid-rows-1 place-items-center">
+					<LoadingScreen />
+				</section>
 			</section>
 		</>
 	);
