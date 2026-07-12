ce257adf28237a7dc21bd17bbf6a64f5fe508c2d
Cheska Huang <julyannahuang@gmail.com>
2026-07-12 11:08:18 +0800

    initial loading bar and disable biome no index as key

--- app/intro/Intro.tsx
+++ app/intro/Intro.tsx
@@ -1,11 +1,28 @@
 export function LoadingBar({ timer }: { timer: number }) {
-	return <></>;
+	const totalSeconds = 30;
+	const totalBars = 18;
+	const updatePercentages = [0, 0.25, 0.75, 1];
+	const updateTimes = updatePercentages.map((percent) =>
+		Math.floor(totalSeconds * percent),
+	);
+	const updateCounts = updatePercentages.map((percent) =>
+		Math.floor(totalBars * percent),
+	);
+	return (
+		<div
+			className={`w-[40vw] h-[8vh] border-[4px] border-white grid grid-cols-${totalBars} grid-rows-1 gap-2 p-2`}
+		>
+			{Array.from({ length: 18 }, (_, i) => (
+				<div key={i} className="bg-white w-full h-full"></div>
+			))}
+		</div>
+	);
 }
 
 export function LoadingScreen() {
 	return (
-		<div className="flex h-screen w-screen items-center justify-center">
-			<p className="font-mono text-white text-xl text-shadow text-center w-1/2">
+		<div className="flex flex-col gap-15 h-screen w-screen items-center justify-center">
+			<p className="font-mono text-white text-2xl text-shadow text-center w-1/2">
 				Trigger warning: This interactive article contains
 				<br />
 				mentions of sexual harassment
