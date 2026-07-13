fe3e99cc82fcf3ea7efd2ee45e42d8cfc08ea19c
Cheska Huang <julyannahuang@gmail.com>
2026-07-14 06:07:15 +0800

    Menubar layout

---
+++ app/layouts/MenuBarLayout.tsx
@@ -0,0 +1,25 @@
+import { Outlet, redirect } from "react-router";
+import { capitalize, dropWhile, join, pipe, takeWhile } from "remeda";
+import { MenuBar } from "../components/MenuBar";
+import type { Route } from "./+types/menubar-layout";
+
+export default function MenuBarLayout({ matches }: Route.ComponentProps) {
+	// get pathname from matches (last match)
+	// take only parent route (drop initial slash and take until next slash)
+	const routePath = matches[matches.length - 1].pathname;
+	const routeName = pipe(
+		routePath.split(""),
+		dropWhile((c) => c === "/"),
+		takeWhile((c) => c !== "/"),
+		join(""),
+		capitalize(),
+	);
+	return (
+		<>
+			<Outlet />
+			<div className="w-screen fixed top-4">
+				<MenuBar title={routeName} closeAction={() => redirect("/desktop")} />
+			</div>
+		</>
+	);
+}
