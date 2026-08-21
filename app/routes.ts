d639e242df804b7c15774d720a45d8735be4d884
angelakyrasalarda-prog <angela.kyra.salarda@student.ateneo.edu>
2026-08-21 14:39:18 +0800

    Merge branch 'main' of https://github.com/theguidon/if-these-walls-could-talk into between-procedure

--- app/routes.ts
+++ app/routes.ts
@@ -7,17 +7,20 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-    layout("./layouts/ScreenLayout.tsx", [
-        index("routes/home.tsx"),
-        layout("./layouts/DesktopLayout.tsx", [
-            route("desktop", "./desktop/Desktop.tsx"),
-            ...prefix("files", [
-                layout("./layouts/BrowserBarLayout.tsx", [
-                    route("public-reckonings", "routes/files/public-reckonings.tsx"),
-                    route("codes-and-complaints", "routes/files/codes-and-complaints.tsx"),
+	layout("./layouts/ScreenLayout.tsx", [
+		index("routes/home.tsx"),
+		layout("./layouts/DesktopLayout.tsx", [
+			route("desktop", "./desktop/Desktop.tsx"),
+			...prefix("files", [
+				layout("./layouts/BrowserBarLayout.tsx", [
+					route("public-reckonings", "routes/files/public-reckonings.tsx"),
+					route(
+						"codes-and-complaints",
+						"routes/files/codes-and-complaints.tsx",
+					),
                     route("procedure-hesitation", "routes/files/ProcedureHesitationContent.tsx"),
-                ]),
-            ]),
-        ]),
-    ]),
+				]),
+			]),
+		]),
+	]),
 ] satisfies RouteConfig;
