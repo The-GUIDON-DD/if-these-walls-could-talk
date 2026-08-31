b299324ff5deb9621c0308d0521458d23a37e029
Cheska Huang <julyannahuang@gmail.com>
2026-08-31 23:50:33 +0800

    fixed biome checks

--- app/desktop/Desktop.tsx
+++ app/desktop/Desktop.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useLocation, Outlet } from "react-router";
+import { Link, Outlet, useLocation } from "react-router";
 import { PopupWindow } from "../components/PopupWindow";
 import {
 	DESKTOP_ICONS,
